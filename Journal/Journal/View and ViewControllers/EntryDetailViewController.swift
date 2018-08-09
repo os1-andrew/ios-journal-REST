5cc9af768e55180c1f2547e377df6d708bd43ab8
Andrew Liao <andrewoliao@gmail.com>
2018-08-09 10:24:47 -0700

    Implement Storyboard and related View/ViewControllers. Connected outlets to respective VCs

---
+++ Journal/Journal/View and ViewControllers/EntryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Andrew Dhan on 8/9/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var textViewLabel: UITextView!
+    @IBOutlet weak var textLabel: UITextField!
+}
---
+++ Journal/Journal/View and ViewControllers/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Andrew Dhan on 8/9/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
