e6d4a28f07e749e05e5e4124e9ae2c389b4a7d51
Andrew Liao <andrewoliao@gmail.com>
2018-08-09 20:34:05 -0700

    Fully implement EntryDetailViewController, save action, and the related methods

--- Journal/Journal/View and ViewControllers/EntryDetailViewController.swift
+++ Journal/Journal/View and ViewControllers/EntryDetailViewController.swift
@@ -10,12 +10,49 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 
+    func updateViews(){
+        if !isViewLoaded { return }
+        
+        switch entry {
+        case nil:
+            self.title = "Create Entry"
+        default:
+            self.title = entry?.title
+            textLabel.text = entry?.title
+            textViewLabel.text = entry?.bodyText
+        }
+
+        
+    }
+    
+    
     @IBAction func save(_ sender: Any) {
+        guard let title = textLabel.text,
+            let bodyText = textViewLabel.text else {return}
+        
+        if title == "" || bodyText == "" {return}
+        
+        if let entry = entry {
+            entryController?.update(for: entry, withTitle: title, bodyText: bodyText) {
+                self.navigationController?.popViewController(animated: true)
+                return nil }
+        } else {
+            entryController?.createEntry(withTitle: title, bodyText: bodyText){
+                self.navigationController?.popViewController(animated: true)
+                return nil}
+        }
+        
+        
+        
     }
     
     @IBOutlet weak var textViewLabel: UITextView!
     @IBOutlet weak var textLabel: UITextField!
     
-    var entry: Entry?
     var entryController: EntryController?
+    var entry: Entry?{
+        didSet{
+            updateViews()
+        }
+    }
 }
